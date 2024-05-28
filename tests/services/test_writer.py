0f0a18331e7d9cefa5c5703e53ec1250c8580892
Laurens Weijs <laurens.weijs@rijksoverheid.nl>
2024-05-28 14:00:01 +0200

    Add unittest

--- tests/services/test_writer.py
+++ tests/services/test_writer.py
@@ -1,26 +1,42 @@
+import pytest
+from yaml import safe_load
 from tad.services.writer import FileSystemWriter, S3Writer, GitWriter
+from pathlib import Path
+
+FILENAME = "test.yaml"
+LOCATION = "./tests/data"
+
+
+@pytest.fixture(autouse=True)
+def cleanup_test_yaml():
+    yield
+    file = Path(LOCATION) / FILENAME
+    if Path.is_file(file):
+        Path.unlink(file)
 
 
 def test_file_system_writer_empty_yaml():
-    data = None
-    location = "./"
-    file_system_writer = FileSystemWriter(data, location)
+    data = {}
+    file_system_writer = FileSystemWriter(data, LOCATION, FILENAME)
     file_system_writer.write()
 
-    # os check if location exists
-    # is content gelijk aan wat we verwachten
-    pass
+    assert Path.is_file(Path(LOCATION) / FILENAME), True
 
 
 def test_file_system_writer_yaml_with_content():
     data = {"test": "test"}
-    location = "./"
-    FileSystemWriter(data, location)
+    file_system_writer = FileSystemWriter(data, LOCATION, FILENAME)
+    file_system_writer.write()
 
-    # os check if location exists
-    # is content gelijk aan wat we verwachten
-    pass
+    with open(Path(LOCATION) / FILENAME, "r") as f:
+        assert safe_load(f) == data, True
 
+
+def test_abstract_writer_non_yaml_filename():
+    data = {"test": "test"}
+    filename = "test.csv"
+    with pytest.raises(ValueError):
+        FileSystemWriter(data, LOCATION, filename)
 
 def test_git_writer():
     pass
