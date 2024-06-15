b1268d9a1c7f086cc98df7fc7ee312599c4dc9b2
Berry den Hartog <38954346+berrydenhartog@users.noreply.github.com>
2024-06-15 21:11:04 +0200

    Inject version into template

--- tad/api/routes/deps.py
+++ tad/api/routes/deps.py
@@ -1,9 +1,15 @@
+from fastapi import Request
 from fastapi.templating import Jinja2Templates
 from jinja2 import Environment
 
 from tad.core.config import settings
 
+
+def version_context_processor(request: Request):
+    return {"version": settings.VERSION}
+
+
 env = Environment(
     autoescape=True,
 )
-templates = Jinja2Templates(directory=settings.TEMPLATE_DIR, env=env)
+templates = Jinja2Templates(directory=settings.TEMPLATE_DIR, context_processors=[version_context_processor], env=env)
