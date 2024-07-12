79466d6416c9b22a35bcaa780bd3a8110651a97f
robbertuittenbroek <robbert.uittenbroek@rijksoverheid.nl>
2024-07-12 14:02:42 +0200

    Update regression test to prove the bug has been fixed

--- tests/regression/e2e/test_duplicate_task_bug.py
+++ tests/regression/e2e/test_duplicate_task_bug.py
@@ -11,8 +11,6 @@ def test_duplicate_task(browser: Page, db: DatabaseTestUtils) -> None:
     resulting in visual duplicates of the card being dragged.
     This test captured that behaviour.
     """
-    error_logs: list[str] = []
-    browser.on("console", lambda msg: error_logs.append(msg.text))
 
     all_status = all_statusses()
     db.given([*all_status])
@@ -20,22 +18,20 @@ def test_duplicate_task(browser: Page, db: DatabaseTestUtils) -> None:
 
     browser.goto("/pages/")
 
-    expect(browser.locator("#column-1 #card-1")).to_be_visible()
+    expect(browser.locator("#column-1 #card-container-1")).to_be_visible()
     expect(browser.locator("#column-3")).to_be_visible()
 
-    browser.evaluate('document.getElementById("cardMovedForm").setAttribute("hx-target","#card-1")')
+    browser.evaluate('document.getElementById("cardMovedForm").setAttribute("hx-target","#card-content-1")')
     browser.evaluate('document.getElementsByName("taskId")[0].value = 1')
     browser.evaluate('document.getElementsByName("statusId")[0].value = 2')
     browser.evaluate('document.getElementsByName("previousSiblingId")[0].value = -1')
     browser.evaluate('document.getElementsByName("nextSiblingId")[0].value = -1')
 
-    browser.locator("#card-1").hover()
+    browser.locator("#card-container-1").hover()
     browser.mouse.down()
     browser.locator("#column-3").hover()
     browser.evaluate('htmx.trigger("#cardMovedForm", "cardmoved")')
     sleep(1)
     browser.mouse.up()
     cards = browser.locator(".progress_card_container").all()
-    assert len(cards) == 2
-
-    expect(browser.locator(".header_logo_container")).to_be_visible()
+    assert len(cards) == 1
