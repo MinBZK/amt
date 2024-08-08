46c567d80c4776841d8b1fca63e719662f58cb84
Berry den Hartog <38954346+berrydenhartog@users.noreply.github.com>
2024-08-08 09:35:55 +0000

    Remove demo mode

--- amt/core/db.py
+++ amt/core/db.py
@@ -1,11 +1,9 @@
 import logging
 
 from sqlalchemy.engine import Engine
-from sqlmodel import Session, SQLModel, create_engine, select, update
+from sqlmodel import Session, SQLModel, create_engine, select
 
 from amt.core.config import get_settings
-from amt.enums.status import Status
-from amt.models import Task, User
 
 logger = logging.getLogger(__name__)
 
@@ -29,17 +27,6 @@ def check_db() -> None:
     logger.info("Finish Checking database connection")
 
 
-def remove_old_demo_objects(session: Session) -> None:
-    task = session.exec(select(Task).where(Task.title == "First task")).first()
-    if task:
-        session.delete(task)
-    session.exec(update(Task).values(status_id=None, user_id=None))  # type: ignore
-    user = session.exec(select(User).where(User.name == "Robbert")).first()
-    if user:
-        session.delete(user)
-    session.commit()
-
-
 def init_db() -> None:
     logger.info("Initializing database")
 
@@ -47,30 +34,4 @@ def init_db() -> None:
         logger.info("Creating database schema")
         SQLModel.metadata.create_all(get_engine())
 
-    with Session(get_engine()) as session:  # pragma: no cover
-        if get_settings().ENVIRONMENT == "demo":
-            logger.info("Creating demo data")
-            remove_old_demo_objects(session)
-            add_demo_users(session, ["default user"])
-            add_demo_tasks(session, Status.TODO, 3)
     logger.info("Finished initializing database")
-
-
-def add_demo_users(session: Session, user_names: list[str]) -> None:
-    for user_name in user_names:
-        user = session.exec(select(User).where(User.name == user_name)).first()
-        if not user:
-            session.add(User(name=user_name, avatar=None))
-    session.commit()
-
-
-def add_demo_tasks(session: Session, status: Status, number_of_tasks: int) -> None:
-    for index in range(1, number_of_tasks + 1):
-        title = "Example task " + str(index)
-        task = session.exec(select(Task).where(Task.title == title)).first()
-        if not task:
-            session.add(
-                Task(title=title, description="Example description " + str(index), sort_order=index, status_id=status)
-            )
-    session.exec(update(Task).values(status_id=status))  # type: ignore
-    session.commit()
--- amt/core/types.py
+++ amt/core/types.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
 # TODO(berry): make enums and convert to types
-EnvironmentType = Literal["local", "production", "demo"]
+EnvironmentType = Literal["local", "production"]
 LoggingLevelType = Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 DatabaseSchemaType = Literal["sqlite", "postgresql", "mysql", "oracle"]
