c0bfcfd2968cb19e31b77d853d3035fb37d0e367
robbertuittenbroek <robbert.uittenbroek@rijksoverheid.nl>
2024-12-18 11:59:13 +0100

    Fix pydantic related errors due to handling None values

--- amt/schema/algorithm.py
+++ amt/schema/algorithm.py
@@ -9,19 +9,19 @@ class AlgorithmBase(BaseModel):
 
 class AlgorithmNew(AlgorithmBase):
     instruments: list[str] | str = []
-    type: str = Field(default=None)
-    open_source: str = Field(default=None)
-    risk_group: str = Field(default=None)
-    conformity_assessment_body: str = Field(default=None)
-    systemic_risk: str = Field(default=None)
-    transparency_obligations: str = Field(default=None)
+    type: str | None = Field(default=None)
+    open_source: str | None = Field(default=None)
+    risk_group: str | None = Field(default=None)
+    conformity_assessment_body: str | None = Field(default=None)
+    systemic_risk: str | None = Field(default=None)
+    transparency_obligations: str | None = Field(default=None)
     role: list[str] | str = []
-    template_id: str = Field(default=None)
+    template_id: str | None = Field(default=None)
     organization_id: int = Field()
 
     @field_validator("organization_id", mode="before")
     @classmethod
-    def ensure_required(cls, v: int | str) -> int:  # noqa
+    def ensure_required(cls, v: int | str) -> int:
         if isinstance(v, str) and v == "":  # this is always a string
             # TODO (Robbert): the error message from pydantic becomes 'Value error,
             #  missing' which is why a custom message will be applied
@@ -31,5 +31,5 @@ class AlgorithmNew(AlgorithmBase):
 
     @field_validator("instruments", "role")
     @classmethod
-    def ensure_list(cls, v: list[str] | str) -> list[str]:  # noqa
+    def ensure_list(cls, v: list[str] | str) -> list[str]:
         return v if isinstance(v, list) else [v]
